ce7f922171b66cc7e032ee3a49348158101ea518
Elona Kajtazi <MY_NAME@example.com>
2022-09-26 16:25:14 +0300

    ....

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,10 @@
 // this is where we are going to have our needed functions
+import bcrypt from "bcryptjs";
+
+export function hash(password: string) {
+  return bcrypt.hashSync(password, 5);
+}
+
+export function verify(password: string, hash: string) {
+  return bcrypt.compareSync(password, hash);
+}
