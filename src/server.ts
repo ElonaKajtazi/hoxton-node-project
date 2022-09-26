703586d28f44fc9dcac53ff4d85b7da0d95c56c3
Elona Kajtazi <MY_NAME@example.com>
2022-09-26 16:17:19 +0300

    created an endpoint to get all books

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,29 @@
-import express from "express"
-import cors from "cors"
-import { PrismaClient } from "@prisma/client"
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-const app =  express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const port = 3333
+const port = 3456;
 
 app.get("/", (req, res) => {
-res.send("Starting project")
-})
+  res.send("Starting project");
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await prisma.book.findMany({ include: { owner: true } });
+    res.send(books);
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ errors: [error.message] });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`App running: http://localhost:${port}`)
-})
+  console.log(`App running: http://localhost:${port}`);
+});
