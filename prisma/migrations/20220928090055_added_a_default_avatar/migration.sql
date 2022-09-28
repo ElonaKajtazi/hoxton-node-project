8a821a555ff2f8dd7cea6fee1b4d14a8c99a4d47
Elona Kajtazi <MY_NAME@example.com>
2022-09-28 12:01:25 +0300

    Added a default avatar for users...

---
+++ prisma/migrations/20220928090055_added_a_default_avatar/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL DEFAULT 500,
+    "avatar" TEXT DEFAULT 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+);
+INSERT INTO "new_User" ("avatar", "balance", "email", "id", "name", "password") SELECT "avatar", "balance", "email", "id", "name", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
